f21bc3871d7a9a97da285aae26ceac0e009ae4a5
amaiuri <alfredomaiuri@gmail.com>
2017-05-07 21:05:36 -0400

    Add files via upload
    Se agrego, RIF, Grado de instrucción, Jefe de familia, pertenece a una etnia indigena, trabaja, profesión u oficio, discapacidad, también se sql_constraints para cedula y rif, ya que estos deben ser únicos por cada persona

--- tcc_personas/modelos/personas.py
+++ tcc_personas/modelos/personas.py
@@ -11,18 +11,29 @@ class tcc_personas(osv.osv):
 		'consejocomunal_id': fields.many2one('tcc.consejocomunales','Consejo Comunal',required=True),
 		'familia_id': fields.many2one('tcc_familia.tcc_familia','Nombre del Grupo Familiar',required=True),
 		'cedula': fields.integer('Cedula',required=True),
+		'rif': fields.char('RIF',size=10),
 		's_nombre': fields.char('Segundo Nombre'),
-		'p_apellido': fields.char('Primer Apellido'),
+		'p_apellido': fields.char('Primer Apellido',required=True),
 		's_apellido': fields.char('Segundo Apellido'),
 		'fecha_nacimiento': fields.date('Fecha de Nacimiento'),
-		'estado_civil': fields.selection([('Soltero','Soltero'),('Casado','Casado'),('Divorsiado','Divorsiado')],'Estado Civil'),
-		'nacionalidad': fields.selection([('Venezolano','Venezolano'),('Estranjero','Estranjero')],'Nacionalidad'),
+		'estado_civil': fields.selection([('soltero','Soltero'),('casado','Casado'),('divorciado','Divorciado')],'Estado Civil'),
+		'nacionalidad': fields.selection([('venezolana','Venezolana'),('extranjera','Extranjera')],'Nacionalidad'),
 		'telefono': fields.char('Telefono'),
 		'sexo': fields.selection([('masculino','Masculino'),('femenino','Femenino')],'Sexo'),
+		'grado_educa': fields.selection([('analfabeta','Analfabeta'),('primaria','Primaria'),('bachiller','Bachiller'),('tsu','TSU'),('universitario','Universitario')],'Grado de instruccion'),
+		'profesion': fields.char('Profesion u oficio'),
+		'discapacidad': fields.boolean('Discapacidad'),
+		'jefe': fields.boolean('Jefe de familia'),
+		'trabaja': fields.boolean('Trabaja'),
+		'etnia': fields.boolean('Pertenece a una etnia indigena'),
 		'status': fields.selection([('vivo','Con Vida'),('muerto','Fallecido')],'Estado'),
 		'active': fields.boolean('Activo',default=True),
 	}
-
+	
+	_sql_constraints=[
+	('cedula_unique','UNIQUE (cedula)','Ya existe una persona con el mismo número de cédula'),
+	('rif_unique','UNIQUE (rif)','Ya existe una persona con el mismo número de RIF')
+	]
 
 	_defaults={
 		'is_persona':True
